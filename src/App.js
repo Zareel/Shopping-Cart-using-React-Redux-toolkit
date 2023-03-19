1d4f2e3b15cb857a825e7367c390f482135bfa40
Zareel Kalam <zareelbwd@gmail.com>
2023-03-19 11:23:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App w-full h-screen bg-[#111111] ">
+      <div className="max-w-4xl mx-auto text-white ">
+        <h1 className="text-3xl font-bold py-10">Redux-toolkit</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
